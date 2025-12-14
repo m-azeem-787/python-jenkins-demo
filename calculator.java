4ea0a1c6f3da1e8058a3c1fdcdc745e7cc4071bd
m_Azeem <m.azeem45101@gmail.com>
2025-12-14 14:44:08 +0500

    .java

---
+++ calculator.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+    }
+}
